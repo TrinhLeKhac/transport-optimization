a8e580fd8e70c81fd0a7d8926a703abda5d1a6df
trinhlk2 <emperorfire52@gmail.com>
2023-12-30 18:43:19 +0700

    Change logic get data ngung_giao_nhan.xlsx

--- scripts/transform/ngung_giao_nhan.py
+++ scripts/transform/ngung_giao_nhan.py
@@ -7,12 +7,8 @@ def transform_data_ngung_giao_nhan():
     # Đọc data ngưng giao nhận
     ngung_giao_nhan_df = pd.read_parquet(ROOT_PATH + '/processed_data/ngung_giao_nhan.parquet')
 
-    # Chọn lấy cột cần thiết và đổi tên cột
-    ngung_giao_nhan_df['status'] = ngung_giao_nhan_df['status'].fillna('Bình thường')
-    ngung_giao_nhan_df['status'] = ngung_giao_nhan_df['status'].apply(
-        lambda s: unidecode(' '.join(s.split()).strip().lower()))
-    ngung_giao_nhan_df.loc[ngung_giao_nhan_df['status'].isin(['chuyen ngoai', 'cham tuyen']), 'status'] = 'Quá tải'
-    ngung_giao_nhan_df.loc[ngung_giao_nhan_df['status'] != 'Quá tải', 'status'] = 'Bình thường'
+    ngung_giao_nhan_df.loc[ngung_giao_nhan_df['status'].isna(), 'status'] = 'Bình thường'
+    ngung_giao_nhan_df.loc[ngung_giao_nhan_df['status'] != 'Bình thường', 'status'] = 'Quá tải'
 
     ngung_giao_nhan_df = (
         PROVINCE_MAPPING_DISTRICT_CROSS_CARRIER_DF.merge(
