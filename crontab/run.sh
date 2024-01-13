49b945d52e1633d8744a1543eeeed87e20875aa5
trinhlk2 <emperorfire52@gmail.com>
2024-01-13 07:17:25 +0700

    Update file run.sh

--- crontab/run.sh
+++ crontab/run.sh
@@ -14,19 +14,19 @@ source ./venv/bin/activate &&
 sudo systemctl stop supervisord &&
 
 # Run script processing data
-python ./scripts/processing/total_processing.py --mode $2 &&
+python ./scripts/processing/total_processing.py --mode $2 --run_date $3 &&
 
 # Run script out data for API
-python ./scripts/output/out_data_api.py &&
+python ./scripts/output/out_data_api.py --run_date $3 &&
 
 # Run script out data for visualization
-python ./scripts/output/out_data_final.py &&
+python ./scripts/output/out_data_final.py --run_date $3 &&
 
 # Run script out data query database
-python ./scripts/output/out_data_query_db.py --mode $1 &&
+python ./scripts/output/out_data_query_db.py --mode $1 --run_date $3 &&
 
 # Run script ingest data to database
-python ./scripts/database/ingest_data.py --mode $1 &&
+python ./scripts/database/ingest_data.py --mode $1 --run_date $3 &&
 
 # Start again module supervisord after updating data
 sudo systemctl start supervisord
