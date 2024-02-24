8305e8714c5f933065a83d4b2db74a3283d0b5bf
trinhlk2 <emperorfire52@gmail.com>
2024-02-24 08:57:19 +0700

    Optimize script get transaction order

--- scripts/processing/giao_dich_full.py
+++ scripts/processing/giao_dich_full.py
@@ -88,7 +88,6 @@ def tong_hop_thong_tin_giao_dich_full(run_date_str, n_days_back=30):
         'picked_at', 'carrier_updated_at', 'last_delivering_at', 'carrier_delivered_at', 'date',
     ]]
 
-    valid_order_df = valid_order_df.sort_values('date', ascending=False).drop_duplicates('order_code', keep='first')
     print('Min (created_at): ', valid_order_df['created_at'].min())
     print('Max (created_at): ', valid_order_df['created_at'].max())
     print('Shape: ', len(valid_order_df))
