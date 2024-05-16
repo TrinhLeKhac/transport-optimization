50c3ea2fc8412f53f56d37ddaa6081bc5a7bb190
trinhlk2 <trinhlk2@fpt.com>
2024-05-17 06:52:31 +0700

    Off mode swith on/off supervisord

--- crontab/run.sh
+++ crontab/run.sh
@@ -49,7 +49,7 @@ python ./scripts/output/out_data_query_db.py --mode $mode &&
 python ./scripts/database/ingest_data.py --mode $mode --run_date $run_date &&
 
 # Start again module supervisord after updating data
-sudo systemctl start supervisord &&
+#sudo systemctl start supervisord &&
 
 # Call python module to alert SUCCESS to telegram group
 python -c "from scripts.utilities.helper import *; telegram_bot_send_success_message()"
