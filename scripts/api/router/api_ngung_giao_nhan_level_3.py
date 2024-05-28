93d1332bf9dee3c3cf509f17ed5395fca54fef84
TrinhLeKhac <emperorfire52@gmail.com>
2024-05-28 21:51:50 +0700

    Update logics API /superai/v2/status

--- scripts/api/router/api_ngung_giao_nhan_level_3.py
+++ scripts/api/router/api_ngung_giao_nhan_level_3.py
@@ -8,39 +8,82 @@ router = APIRouter()
 
 
 @router.post("")
-def calculate(carrier_id: Optional[int] = None, commune: Optional[str] = None):
+def calculate(carrier_id: Optional[int] = None, district: Optional[str] = None, commune: Optional[str] = None):
 
     # Create connection
     connection = psycopg2.connect(
         settings.SQLALCHEMY_DATABASE_URI
     )
     cursor = connection.cursor()
-    if commune is None:
-        if carrier_id is None:
-            table_query = """
-                select carrier_id, 
-                receiver_province_code as province, 
-                receiver_district_code as district, 
-                CASE 
-                    WHEN status = 'Bình thường' THEN '0'
-                    WHEN status = 'Quá tải' THEN '1' 
-                END AS status
-                FROM db_schema.tbl_ngung_giao_nhan;
-            """
-        else:
+
+    table_query = ""
+
+    if district is None and commune is None:
+        return {
+            "error": True,
+            "message": "Please choose district or commune to reduce workload of API",
+            "data": [],
+        }
+
+    if district:
+        if carrier_id is None and commune is None:
             table_query = f"""
                 select carrier_id, 
                 receiver_province_code as province, 
                 receiver_district_code as district, 
+                receiver_commune_code as commune,
                 CASE 
                     WHEN status = 'Bình thường' THEN '0'
                     WHEN status = 'Quá tải' THEN '1' 
                 END AS status
-                FROM db_schema.tbl_ngung_giao_nhan
-                WHERE tbl_ngung_giao_nhan.carrier_id = '{carrier_id}';
+                FROM db_schema.tbl_ngung_giao_nhan_level_3
+                WHERE tbl_ngung_giao_nhan_level_3.receiver_district_code = '{district}';
             """
-    else:
-        if carrier_id is None:
+        if carrier_id is not None and commune is None:
+            table_query = f"""
+                select carrier_id, 
+                receiver_province_code as province, 
+                receiver_district_code as district, 
+                receiver_commune_code as commune,
+                CASE 
+                    WHEN status = 'Bình thường' THEN '0'
+                    WHEN status = 'Quá tải' THEN '1' 
+                END AS status
+                FROM db_schema.tbl_ngung_giao_nhan_level_3
+                WHERE tbl_ngung_giao_nhan_level_3.carrier_id = '{carrier_id}'
+                AND tbl_ngung_giao_nhan_level_3.receiver_district_code = '{district}';
+            """
+        if carrier_id is None and commune is not None:
+            table_query = f"""
+                select carrier_id, 
+                receiver_province_code as province, 
+                receiver_district_code as district, 
+                receiver_commune_code as commune,
+                CASE 
+                    WHEN status = 'Bình thường' THEN '0'
+                    WHEN status = 'Quá tải' THEN '1' 
+                END AS status
+                FROM db_schema.tbl_ngung_giao_nhan_level_3
+                WHERE tbl_ngung_giao_nhan_level_3.receiver_district_code = '{district}'
+                AND tbl_ngung_giao_nhan_level_3.receiver_commune_code = '{commune}';
+            """
+        if carrier_id is not None and commune is not None:
+            table_query = f"""
+                select carrier_id, 
+                receiver_province_code as province, 
+                receiver_district_code as district, 
+                receiver_commune_code as commune,
+                CASE 
+                    WHEN status = 'Bình thường' THEN '0'
+                    WHEN status = 'Quá tải' THEN '1' 
+                END AS status
+                FROM db_schema.tbl_ngung_giao_nhan_level_3
+                WHERE tbl_ngung_giao_nhan_level_3.carrier_id = '{carrier_id}'
+                AND tbl_ngung_giao_nhan_level_3.receiver_district_code = '{district}'
+                AND tbl_ngung_giao_nhan_level_3.receiver_commune_code = '{commune}';
+            """
+    if commune:
+        if carrier_id is None and district is None:
             table_query = f"""
                 select carrier_id, 
                 receiver_province_code as province, 
@@ -53,7 +96,7 @@ def calculate(carrier_id: Optional[int] = None, commune: Optional[str] = None):
                 FROM db_schema.tbl_ngung_giao_nhan_level_3
                 WHERE tbl_ngung_giao_nhan_level_3.receiver_commune_code = '{commune}';
             """
-        else:
+        if carrier_id is not None and district is None:
             table_query = f"""
                 select carrier_id, 
                 receiver_province_code as province, 
@@ -67,16 +110,42 @@ def calculate(carrier_id: Optional[int] = None, commune: Optional[str] = None):
                 WHERE tbl_ngung_giao_nhan_level_3.carrier_id = '{carrier_id}'
                 AND tbl_ngung_giao_nhan_level_3.receiver_commune_code = '{commune}';
             """
+        if carrier_id is None and district is not None:
+            table_query = f"""
+                select carrier_id, 
+                receiver_province_code as province, 
+                receiver_district_code as district, 
+                receiver_commune_code as commune,
+                CASE 
+                    WHEN status = 'Bình thường' THEN '0'
+                    WHEN status = 'Quá tải' THEN '1' 
+                END AS status
+                FROM db_schema.tbl_ngung_giao_nhan_level_3
+                WHERE tbl_ngung_giao_nhan_level_3.receiver_district_code = '{district}'
+                AND tbl_ngung_giao_nhan_level_3.receiver_commune_code = '{commune}';
+            """
+        if carrier_id is not None and district is not None:
+            table_query = f"""
+                select carrier_id, 
+                receiver_province_code as province, 
+                receiver_district_code as district, 
+                receiver_commune_code as commune,
+                CASE 
+                    WHEN status = 'Bình thường' THEN '0'
+                    WHEN status = 'Quá tải' THEN '1' 
+                END AS status
+                FROM db_schema.tbl_ngung_giao_nhan_level_3
+                WHERE tbl_ngung_giao_nhan_level_3.carrier_id = '{carrier_id}'
+                AND tbl_ngung_giao_nhan_level_3.receiver_district_code = '{district}'
+                AND tbl_ngung_giao_nhan_level_3.receiver_commune_code = '{commune}';
+            """
+
     cursor.execute(table_query)
     rows = cursor.fetchall()
 
     # Get the field names from the Pydantic model
-    if commune is None:
-        field_names = schemas.NGNModel.__annotations__.keys()
-        result = [schemas.NGNModel(**dict(zip(field_names, row))) for row in rows]
-    else:
-        field_names = schemas.NGNLV3Model.__annotations__.keys()
-        result = [schemas.NGNLV3Model(**dict(zip(field_names, row))) for row in rows]
+    field_names = schemas.NGNLV3Model.__annotations__.keys()
+    result = [schemas.NGNLV3Model(**dict(zip(field_names, row))) for row in rows]
 
     # Commit the transaction
     connection.commit()
