10f6399ccc502e6ceaf869d755b707f5c613682b
TrinhLeKhac <emperorfire52@gmail.com>
2024-12-04 23:10:48 +0700

    Update logics tuyen_uu_tien

--- scripts/output/out_data_tuyen_uu_tien.py
+++ scripts/output/out_data_tuyen_uu_tien.py
@@ -20,13 +20,10 @@ FINAL_FULL_COLS = [
     'order_type', 'order_type_id', 'sys_order_type_id',
     'orders_in_1_month', 'ndays_in_1_month',
     'orders_in_2_month', 'ndays_in_2_month',
-    'orders_in_3_month', 'ndays_in_3_month',
-    'orders_in_6_month', 'ndays_in_6_month',
-    'orders_in_12_month', 'ndays_in_12_month',
 ]
 
 API_COLS = [
-    'preferred_carrier_id',
+    'carrier_id',
     'sender_province_code', 'sender_district_code',
     'receiver_province_code', 'receiver_district_code',
     'order_type_id', 'sys_order_type_id',
@@ -38,7 +35,6 @@ def get_total_order():
     df_total_order_old = pd.read_parquet(ROOT_PATH + '/processed_data/total_order.parquet')
     df_order_new = pd.read_parquet(ROOT_PATH + '/processed_data/order.parquet')
     df_total_order = pd.concat([df_total_order_old, df_order_new]).drop_duplicates().reset_index(drop=True)
-    df_total_order['sys_order_type_id'] = df_total_order['sys_order_type_id'].astype('int')
 
     print('Shape before: ', len(df_total_order_old))
     print('Shape after: ', len(df_total_order))
@@ -46,20 +42,21 @@ def get_total_order():
     df_total_order.to_parquet(ROOT_PATH + '/processed_data/total_order.parquet', index=False)
 
 
-def get_top_percent(group, sort_col, ascending=True, pct=0.2):
+def get_top_percent(group, sort_col, ascending=True, pct=0.25):
     sorted_group = group.sort_values(by=sort_col, ascending=ascending)
     top_n = max(1, int(len(sorted_group) * pct))
     return sorted_group.head(top_n)
 
 
-def get_metadata_tuyen_uu_tien(run_date_str, n_months_back=12):
+def get_metadata_tuyen_uu_tien(run_date_str, n_months_back=2):
     run_date = datetime.strptime(run_date_str, '%Y-%m-%d')
 
     df_target = pd.read_parquet(ROOT_PATH + '/processed_data/total_order.parquet')
     df_target = df_target.loc[
-        (df_target['date'] >= (run_date - relativedelta(months=n_months_back)))
-        & df_target['picked_at'].notna()
+        df_target['picked_at'].notna()
         & df_target['last_delivering_at'].notna()
+        & (df_target['last_delivering_at'] >= (run_date - relativedelta(months=n_months_back)))
+        & (df_target['last_delivering_at'] < run_date)
         ]
     df_target['delta_hour'] = (df_target['last_delivering_at'] - df_target['picked_at']).dt.total_seconds() / 60 / 60
 
@@ -92,56 +89,36 @@ def get_metadata_tuyen_uu_tien(run_date_str, n_months_back=12):
     meta_priority_route_df.to_parquet(ROOT_PATH + '/processed_data/meta_priority_route.parquet', index=False)
 
 
-@exception_wrapper
-def get_data_tuyen_uu_tien(run_date_str, n_months_back=12, priority_type='top 20%'):
+def get_data_tuyen_uu_tien(run_date_str, n_months_back=2, priority_type='top 25%'):
     run_date = datetime.strptime(run_date_str, '%Y-%m-%d')
 
     print('1. Xử lý data giao dịch...')
     df_target = pd.read_parquet(ROOT_PATH + '/processed_data/total_order.parquet')
     df_target = df_target.loc[
-        (df_target['date'] >= (run_date - relativedelta(months=n_months_back)))
-        & df_target['picked_at'].notna()
+        df_target['picked_at'].notna()
         & df_target['last_delivering_at'].notna()
+        & (df_target['last_delivering_at'] >= (run_date - relativedelta(months=n_months_back)))
+        & (df_target['last_delivering_at'] < run_date)
         ]
     df_target['delta_hour'] = (df_target['last_delivering_at'] - df_target['picked_at']).dt.total_seconds() / 60 / 60
 
     df_target['is_1_month'] = False
     df_target['is_2_month'] = False
-    df_target['is_3_month'] = False
-    df_target['is_6_month'] = False
-    df_target['is_12_month'] = False
 
-    df_target['day_picked_at'] = df_target['picked_at'].dt.date
+    df_target['day_last_delivering_at'] = df_target['last_delivering_at'].dt.date
 
     df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=1))), 'is_1_month'] = True
+            df_target['last_delivering_at'] >= (run_date - relativedelta(months=1))), 'is_1_month'] = True
     df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=1))), 'day_picked_at_1m'] = \
-    df_target['day_picked_at']
+            df_target['last_delivering_at'] >= (run_date - relativedelta(months=1))), 'day_last_delivering_at_1m'] = \
+        df_target['day_last_delivering_at']
 
     df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=2))), 'is_2_month'] = True
+            df_target['last_delivering_at'] >= (run_date - relativedelta(months=2))), 'is_2_month'] = True
     df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=2))), 'day_picked_at_2m'] = \
-    df_target['day_picked_at']
+            df_target['last_delivering_at'] >= (run_date - relativedelta(months=2))), 'day_last_delivering_at_2m'] = \
+        df_target['day_last_delivering_at']
 
-    df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=3))), 'is_3_month'] = True
-    df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=3))), 'day_picked_at_3m'] = \
-    df_target['day_picked_at']
-
-    df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=6))), 'is_6_month'] = True
-    df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=6))), 'day_picked_at_6m'] = \
-    df_target['day_picked_at']
-
-    df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=12))), 'is_12_month'] = True
-    df_target.loc[(df_target['last_delivering_at'] < run_date) & (
-                df_target['last_delivering_at'] >= (run_date - relativedelta(months=12))), 'day_picked_at_12m'] = \
-    df_target['day_picked_at']
     print('Done\n')
 
     print('2. Lấy metadata tuyến ưu tiên...')
@@ -155,7 +132,7 @@ def get_data_tuyen_uu_tien(run_date_str, n_months_back=12, priority_type='top 20
     priority_df = df_target_modified.loc[
         (df_target_modified['delta_hour'] <= df_target_modified['idea_delta_hour'])
         & (df_target_modified['picked_at'].dt.hour < 18)
-        & (df_target_modified['last_delivering_at'].dt.hour < 9)
+        & (df_target_modified['last_delivering_at'].dt.hour < 10)
         ]
 
     priority_df.to_parquet(ROOT_PATH + '/processed_data/order_with_priority_route_streamlit.parquet', index=False)
@@ -166,15 +143,9 @@ def get_data_tuyen_uu_tien(run_date_str, n_months_back=12, priority_type='top 20
                   'receiver_district_code', 'order_type_id', 'sys_order_type_id'])
         .agg(
             orders_in_1_month=('is_1_month', 'sum'),
-            ndays_in_1_month=('day_picked_at_1m', 'nunique'),
+            ndays_in_1_month=('day_last_delivering_at_1m', 'nunique'),
             orders_in_2_month=('is_2_month', 'sum'),
-            ndays_in_2_month=('day_picked_at_2m', 'nunique'),
-            orders_in_3_month=('is_3_month', 'sum'),
-            ndays_in_3_month=('day_picked_at_3m', 'nunique'),
-            orders_in_6_month=('is_6_month', 'sum'),
-            ndays_in_6_month=('day_picked_at_6m', 'nunique'),
-            orders_in_12_month=('is_12_month', 'sum'),
-            ndays_in_12_month=('day_picked_at_12m', 'nunique'),
+            ndays_in_2_month=('day_last_delivering_at_2m', 'nunique'),
         ).reset_index()
     )
     print('Done\n')
@@ -183,9 +154,6 @@ def get_data_tuyen_uu_tien(run_date_str, n_months_back=12, priority_type='top 20
     priority_columns = [
         "ndays_in_1_month", "orders_in_1_month",
         "ndays_in_2_month", "orders_in_2_month",
-        "ndays_in_3_month", "orders_in_3_month",
-        "ndays_in_6_month", "orders_in_6_month",
-        "ndays_in_12_month", "orders_in_12_month"
     ]
 
     print('4. Chọn nhà vận chuyển ưu tiên hơn trong cùng tuyến...')
@@ -234,8 +202,15 @@ def get_data_tuyen_uu_tien(run_date_str, n_months_back=12, priority_type='top 20
             ['sender_province_code', 'sender_district_code', 'receiver_province_code', 'receiver_district_code',
              'order_type', 'carrier'])
     )
-    api_priority_df = final_priority_df[API_COLS].drop_duplicates().rename(
-        columns={'preferred_carrier_id': 'carrier_id', 'order_type_id': 'new_type', 'sys_order_type_id': 'route_type'})
+    api_priority_df = (
+        final_priority_df.loc[final_priority_df[
+            ['sender_province_code', 'sender_district_code', 'receiver_province_code',
+             'receiver_district_code']].duplicated(keep=False)]
+        [API_COLS].rename(columns={
+            'order_type_id': 'new_type',
+            'sys_order_type_id': 'route_type'
+        })
+    )
 
     final_priority_df.to_parquet(ROOT_PATH + '/output/priority_route_full.parquet', index=False)
     api_priority_df.to_parquet(ROOT_PATH + '/output/priority_route_api.parquet', index=False)
@@ -253,8 +228,8 @@ if __name__ == '__main__':
     )
     options, args = parser.parse_args()
 
-    n_months_back = 12
-    priority_type = 'top 20%'
+    n_months_back = 2
+    priority_type = 'top 25%'
 
     get_total_order()
     get_metadata_tuyen_uu_tien(options.run_date, n_months_back=n_months_back)
